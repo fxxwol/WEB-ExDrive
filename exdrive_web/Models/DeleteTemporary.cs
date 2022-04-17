2296eb32c176f981a334091514254e627f3ea81b
Andrii Kozubenko <Andrii.Kozubenko.IT.2021@lpnu.ua>
2022-04-17 22:45:05 +0300

    Introduces automatic temporary files system

---
+++ exdrive_web/Models/DeleteTemporary.cs
@@ -0,0 +1,30 @@
+﻿using Azure.Storage.Blobs;
+using JWTAuthentication.Authentication;
+using Microsoft.EntityFrameworkCore;
+
+namespace exdrive_web.Models
+{
+    public class DeleteTemporary
+    {
+        private static string connectionString = "DefaultEndpointsProtocol=https;AccountName=exdrivefiles;AccountKey=zW8bG071a7HbJ4+D5Pxruz4rL47KEx0XwExd7m5CmYtCNdu8A71/rVvvY/ld8hwJ4nObLnAcDB27KZV/0L92TA==;EndpointSuffix=core.windows.net";
+        private static string containerName = "botfiles";
+        public static void DeleteTemporaryFiles()
+        {
+            BlobContainerClient containerClient = new BlobContainerClient(connectionString, containerName);
+            IEnumerable<Azure.Storage.Blobs.Models.BlobItem> blobs = containerClient.GetBlobs().SkipWhile(x => x.Properties.LastModified >= DateTime.UtcNow.AddDays(-1 * 7));
+            //IEnumerable<Azure.Storage.Blobs.Models.BlobItem> blobs = containerClient.GetBlobs().SkipWhile(x => x.Properties.LastModified < DateTime.UtcNow.AddDays(-1 * 7));
+            var optionsBuilder = new DbContextOptionsBuilder<ApplicationDbContext>();
+            optionsBuilder.UseSqlServer("Server=tcp:exdrive.database.windows.net,1433;Initial Catalog=Exdrive;Persist Security Info=False;User ID=fxxwol;Password=AbCD.123;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
+            using (var _context = new ApplicationDbContext(optionsBuilder.Options))
+            {
+                foreach (Azure.Storage.Blobs.Models.BlobItem blob in blobs)
+                {
+                    containerClient.DeleteBlobAsync(blob.Name).Wait();
+                    var todelete = _context.Roles.Find(blob.Name);
+                    if (todelete != null)
+                        _context.Roles.Remove(todelete);
+                }
+            }
+        }
+    }
+}
---
+++ exdrive_web/Models/DeleteTemporaryTimer.cs
@@ -0,0 +1,22 @@
+﻿using System.Timers;
+
+namespace exdrive_web.Models
+{
+    public class DeleteTemporaryTimer
+    {
+        public static System.Timers.Timer aTimer;
+        static double interval24hours = 60 * 60 * 24 * 1000;
+
+        public static void SetTimer()
+        {
+            aTimer = new System.Timers.Timer(interval24hours);
+            aTimer.Elapsed += new ElapsedEventHandler(checkForTime_Elapsed);
+            aTimer.AutoReset = true;
+            aTimer.Enabled = true;
+        }
+        private static void checkForTime_Elapsed(object sender, ElapsedEventArgs e)
+        {
+              DeleteTemporary.DeleteTemporaryFiles();
+        }
+    }
+}
