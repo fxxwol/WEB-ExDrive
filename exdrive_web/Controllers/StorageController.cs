65611109b7dcd9124e52303e4fb689a26e8bdca8
fxxwol <90700503+fxxwol@users.noreply.github.com>
2022-04-17 23:47:48 +0300

    access storage

---
+++ exdrive_web/Controllers/StorageController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace exdrive_web.Controllers
+{
+    [Authorize]
+    public class StorageController : Controller
+    {
+        public IActionResult AccessStorage()
+        {
+            return View();
+        }
+    }
+}
