48ef9028fe2d5ed60b5f9bd7e6115b6989179f19
Andrii Kozubenko <Andrii.Kozubenko.IT.2021@lpnu.ua>
2022-04-22 11:52:04 +0300

    Added link show-up

--- exdrive_web/Controllers/StorageController.cs
+++ exdrive_web/Controllers/StorageController.cs
@@ -24,33 +24,37 @@ namespace exdrive_web.Controllers
         [RequestFormLimits(MultipartBodyLengthLimit = 629145600)]
         public async Task<IActionResult> SingleFile(IFormFile file)
         {
-            var dir = _webHostEnvironment.ContentRootPath;
-            string filename = file.FileName;
-            string format = "";
-            for (int i = filename.LastIndexOf('.'); i < filename.Length; i++)
-                format += file.FileName.ElementAt(i);
-
-            string newname = Guid.NewGuid().ToString() + format;
-            using (var fileStream = new FileStream(Path.Combine(dir, newname), FileMode.Create, FileAccess.Write))
+            if (file != null)
             {
-                file.CopyTo(fileStream);
+                var dir = _webHostEnvironment.ContentRootPath;
+                string filename = file.FileName;
+
+                string format = "";
+                for (int i = filename.LastIndexOf('.'); i < filename.Length; i++)
+                    format += file.FileName.ElementAt(i);
+
+                string newname = Guid.NewGuid().ToString() + format;
+                using (var fileStream = new FileStream(Path.Combine(dir, newname), FileMode.Create, FileAccess.Write))
+                {
+                    file.CopyTo(fileStream);
+                }
+
+                MemoryStream ms = new MemoryStream();
+                var filems = file.OpenReadStream();
+                filems.CopyToAsync(ms).Wait();
+
+                Files files = new Files();
+                files.Name = filename;
+                files.FilesId = newname;
+                files.IsTemporary = true;
+                files.HasAccess = "*";
+
+
+                string downloadLink = "https://exdrivefiles.blob.core.windows.net/botfiles/" + files.FilesId;
+                await UploadAsync.UploadFileAsync(file, dir, files, ms);
+                TempData["AlertMessage"] = downloadLink;
             }
-
-            MemoryStream ms = new MemoryStream();
-            var filems = file.OpenReadStream();
-            filems.CopyToAsync(ms).Wait();
-
-            Files files = new Files();
-            files.Name = filename;
-            files.FilesId = newname;
-            files.IsTemporary = true;
-            files.HasAccess = "*";
-
-
-            string downloadLink = "https://exdrivefiles.blob.core.windows.net/botfiles/" + files.FilesId;
-            await UploadAsync.UploadFileAsync(file, dir, files, ms);
-            TempData["AlertMessage"] = downloadLink;
-
+            
             return RedirectToAction("Index", "Home");
         }
     }
