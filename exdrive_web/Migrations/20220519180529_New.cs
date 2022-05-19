82ed6becb015d215a1a0a4a54c187e7474f339db
fxxwol <90700503+fxxwol@users.noreply.github.com>
2022-05-19 21:13:31 +0300

    New database

---
+++ exdrive_web/Migrations/20220519180529_New.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace exdrive_web.Migrations
+{
+    public partial class New : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "Favourite",
+                table: "Files",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Favourite",
+                table: "Files");
+        }
+    }
+}
