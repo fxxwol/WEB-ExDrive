35fc54bb3804b56e6c3da66d7f328a2ad3d5eb44
Senichkaa <super-kapanaiko406@ukr.net>
2022-05-20 22:21:27 +0300

    storage buttons placement and placement of "favorite" window

--- exdrive_web/Controllers/StorageController.cs
+++ exdrive_web/Controllers/StorageController.cs
@@ -79,7 +79,7 @@ namespace exdrive_web.Controllers
         {
             return View(new UploadInstance());
         }
-        [HttpPost]
+        
         public ActionResult Favourite()
         {
             _userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
